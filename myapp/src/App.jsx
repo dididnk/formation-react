2ae55d5e6f5f5c3bd23b9f8ee45ad3b92ff37b8c
Emmanuel Ngbame <ngbamedistingue@yahoo.com>
2025-07-20 14:01:08 +0200

    feat: useState avec tableau

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -4,20 +4,39 @@ import './App.css';
 
 function App() {
   // state (état, données)
-  const [compteur, setcompteur] = useState(1);
+  const [streets, setStreets] = useState([
+    {id: 1, title: "Paris"},
+    {id: 2, title: "Lyon"},
+    {id: 3, title: "Marseille"},
+    {id: 4, title: "Bordeaux"},
+    {id: 5, title: "Toulouse"},
+    {id: 6, title: "Nice"},
+  ]);
 
   // comportements
-  const handlerClick = () =>{
-    setcompteur(compteur + 1);
+  const hendleDeleteStreet = (id) =>{
+    //1. copie du state
+    const newStreets = [...streets];
+
+    //2. manipuler le state
+    const streetsUpdated = newStreets.filter(street => street.id !== id);
+
+    //3. modifier le state
+    setStreets(streetsUpdated);
   }
+
 
   // affichage (UI, rendu)
   return (
     <>
-      <h1>Compteur {compteur}</h1>
-      <button onClick={handlerClick}>Incrémenter</button>
+      <h1>Exercice : Liste des ville de France</h1>
+      <ol>
+        {streets.map((street)=>{
+          return <li key={street.id}>{street.title}<button onClick={()=>hendleDeleteStreet(street.id)}>X</button> </li>
+        })}
+      </ol>
     </>
   )
 }
 
-export default App
+export default App;
